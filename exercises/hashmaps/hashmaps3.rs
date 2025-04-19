547385c3a3bf9a813ad32e2e33e3905f8418b838
Korialt <dentriffemai@gmail.com>
2025-04-19 17:25:57 +0800

    hashmap3

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -27,10 +27,6 @@ struct Team {
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
-    let default: Team = Team {
-        goals_scored: 0,
-        goals_conceded: 0
-    };
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
@@ -42,8 +38,26 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        scores.entry(team_1_name, default);
-        scores.entry(team_2_name, default);
+        scores.entry(team_1_name).and_modify(
+            |team|{
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        }).or_insert(
+            Team{
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score
+            }
+        );
+        scores.entry(team_2_name).and_modify(
+            |team|{
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+        }).or_insert(
+            Team{
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score
+            }
+        );
         
         
     }
