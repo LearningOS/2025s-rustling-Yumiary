3a1f26826b339c5824122bd79c5462575805df61
Korialt <dentriffemai@gmail.com>
2025-04-21 14:52:39 +0800

    ninth commit -half errors

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -22,17 +22,21 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 /// # Safety
-///
 /// The `address` must contain a mutable reference to a valid `u32` value.
-unsafe fn modify_by_address(address: usize) {
+unsafe fn modify_by_address(mut address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
-    unsafe {
-        todo!("Your code goes here")
+    // SATETY: The caller guarantees that 'address' is a valid pointer to a mutable u32.
+    // Therefore, dereferencing the raw pointer is safe here.
+    unsafe {        
+        let ptr = address as *mut u32;
+        *ptr = 0xAABBCCDD;
+        // todo!("Your code goes here");
+        
     }
 }
 
